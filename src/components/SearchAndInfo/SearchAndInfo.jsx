f02f880cb607f149827b8ab159bf27bdd91b68d6
iijawhor <jawhorali1@gmail.com>
2024-01-18 20:03:26 +0530

    Changes in style

--- src/components/SearchAndInfo/SearchAndInfo.jsx
+++ src/components/SearchAndInfo/SearchAndInfo.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import "./SearchAndInfo.css";
-import { Container, Sear, SearchCompoenent } from "../Exports/exports";
+import {
+  Container,
+  SearchCompoenent,
+  GlobalConnectCities
+} from "../Exports/exports";
 
 function SearchAndInfo() {
   console.log("Rendering search Component............");
   return (
     <Container className="searchAndInfo">
       <SearchCompoenent />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum obcaecati
-      repellat est molestias quaerat dolorem, omnis asperiores maiores sit quam,
-      ab nobis doloribus, eaque hic perferendis. Veritatis sed laborum rerum ut
-      distinctio animi aliquam aperiam ipsum at reiciendis nemo, ducimus
-      corporis nisi! Vel, dolores incidunt quae porro delectus quisquam quaerat
-      consectetur, nemo accusamus aspernatur velit corporis nulla itaque debitis
-      deserunt possimus numquam reprehenderit. Provident deleniti laborum a quo
-      perspiciatis sequi soluta fuga impedit, placeat autem totam, optio
-      obcaecati dolor ducimus fugit! Incidunt in, aliquid vitae sapiente quos
-      alias voluptate nostrum tenetur exercitationem pariatur, laborum nemo,
-      adipisci sequi possimus? Provident, maxime.]
+      <div className="searchInfoContainer">
+        <div className="xyz"></div>
+        <GlobalConnectCities />
+      </div>
     </Container>
   );
 }

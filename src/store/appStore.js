c3c3aad2532af4e5f2f8f792a6804f00d2c1ea53
iijawhor <jawhorali1@gmail.com>
2024-01-20 18:32:35 +0530

    added searchSlice to appStore

--- src/store/appStore.js
+++ src/store/appStore.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
+import searchSlice from "./searchSlice";
 
 export const store = configureStore({
-  reducer: { auth: authSlice }
+  reducer: { auth: authSlice, search: searchSlice }
 });

f641c78d149c1807ec8334848a6fa3971a2f1223
iijawhor <jawhorali1@gmail.com>
2024-01-20 19:30:41 +0530

    Changes in AuthLayout logic to navigate correctly (need to work more)

--- src/components/AuthLayout/AuthLayout.js
+++ src/components/AuthLayout/AuthLayout.js
@@ -3,18 +3,19 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 function AuthLayout({ children, authentication = true }) {
-  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
   const authStatus = useSelector((state) => state.auth.status);
   useEffect(() => {
-    if (authentication && !authStatus) {
-      navigate("/login");
-    } else if (!authentication && authStatus) {
+    if (!authentication && authStatus) {
+      navigate("/login-page");
+    } else if (authentication && authStatus) {
       navigate("/");
     }
     setLoading(false);
   }, [navigate, authStatus, authentication]);
-  return loading ? <h1>{loading}</h1> : <div>{children}</div>;
+
+  return loading ? <h1>Loading...</h1> : <>{children}</>;
 }
 
 export default AuthLayout;

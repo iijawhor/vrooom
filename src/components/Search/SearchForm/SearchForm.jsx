28bda618d62a6ddad48e94deed9e81267257b600
iijawhor <jawhorali1@gmail.com>
2024-01-20 18:35:59 +0530

    Implement SearchForm component with Redux integration
    Implemented a SearchForm component that includes input fields for 'currentLocation' and 'enterDestination,' along with a search button. Utilized React hooks, such as useState, useEffect, and useMemo, to manage component state and debounce user input. Integrated the UseDebounce custom hook for debouncing input changes before triggering the search. Additionally, incorporated Redux to dispatch 'searchData' actions and update the store with filtered results.
    
    Changes:
    - Created SearchForm component with input fields and a search button.
    - Used React hooks for managing state and debouncing user input.
    - Integrated the UseDebounce hook for debouncing input changes.
    - Utilized Redux to dispatch 'searchData' actions and update the store with filtered results.
    
    Note: Ensure that the 'searchData' action is appropriately handled in the Redux store, and consider updating the 'UseDebounce' hook if needed.

--- src/components/Search/SearchForm/SearchForm.jsx
+++ src/components/Search/SearchForm/SearchForm.jsx
@@ -1,21 +1,60 @@
-import React, { useState, forwardRef } from "react";
+import React, { useState, forwardRef, useEffect, useMemo } from "react";
+
 import "./SearchForm.css";
 import "./SearchFormResponsive.css";
-import { LocationSearchingIcon, InputComponent } from "../../Exports/exports";
-
+import { useDispatch, useSelector } from "react-redux";
+import {
+  LocationSearchingIcon,
+  InputComponent,
+  carBookings,
+  UseDebounce
+} from "../../Exports/exports";
+import { searchData } from "../../../store/searchSlice";
 const SearchForm = forwardRef(({ placeholder, buttonTitle }, ref) => {
   const [currentLocation, setCurrentLocation] = useState("");
   const [enterDestination, setEnterDestination] = useState("");
-  const [searchInput, setSearchInput] = useState("");
+  const [searchDatas, setSearchDatas] = useState([]);
+  const dispatch = useDispatch();
+  const debounce = UseDebounce({ currentLocation, enterDestination }, 200);
+  const filter = useMemo(() => {
+    return carBookings.filter((data) => {
+      const pickupLocation = data.currentLocation
+        .toLowerCase()
+        .includes(currentLocation.toLowerCase());
+      const dropLocation = data.destination
+        .toLowerCase()
+        .includes(enterDestination.toLowerCase());
+      return pickupLocation && dropLocation;
+    });
+  }, [currentLocation, enterDestination]);
+
+  // useEffect(() => {
+  //   if (currentLocation && enterDestination) {
+  //     setSearchResult(filter);
+  //   } else {
+  //     setSearchResult([]);
+  //   }
+  // }, [currentLocation, enterDestination, filter]);
 
   const handleSearch = (e) => {
     e.preventDefault();
-    console.log("Search Button Clicked");
+    if (!currentLocation && !enterDestination) {
+      alert("Please enter something");
+      return;
+    } else if (
+      (!currentLocation && enterDestination) ||
+      (currentLocation && !enterDestination)
+    ) {
+      alert("Please enter query");
+      return;
+    }
+    setSearchDatas(filter);
+    dispatch(searchData(filter));
   };
 
   return (
     <>
-      <form action="" className="searchForm" ref={ref}>
+      <form action="" onSubmit={handleSearch} className="searchForm" ref={ref}>
         <div className="searchComponentInputs">
           <div className="searchComponentInputBox">
             <div className="currentLocationDot dot" />
@@ -25,6 +64,7 @@ const SearchForm = forwardRef(({ placeholder, buttonTitle }, ref) => {
               onChange={(e) => setCurrentLocation(e.target.value)}
               value={currentLocation}
               className="searchInput"
+              required="required"
             />
             <LocationSearchingIcon
               className="locationSearchingIcon"
@@ -39,11 +79,12 @@ const SearchForm = forwardRef(({ placeholder, buttonTitle }, ref) => {
               onChange={(e) => setEnterDestination(e.target.value)}
               value={enterDestination}
               className="searchInput"
+              required="required"
             />
           </div>
         </div>
         {/* <div className="searchComponentInputs"> */}
-        <button onClick={handleSearch} className="searchCabs">
+        <button type="submit" className="searchCabs">
           {buttonTitle}
         </button>
       </form>

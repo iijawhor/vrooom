ed7d7fe4c084f2a8df89640bdb9c76b2c95feee0
iijawhor <jawhorali1@gmail.com>
2024-01-23 22:54:47 +0530

    Added some component to render

--- src/components/HomeCompnent/HomeComponent.jsx
+++ src/components/HomeCompnent/HomeComponent.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import "./HomeComponent.css";
 import {
   SearchAndInfo,
   HomeBackground,
-  Container
+  Container,
+  FeaturesDisplay,
+  AskedQuestion,
+  FooterComponent
 } from "../../components/Exports/exports";
 function HomeComponent() {
   return (
     <Container className="homeComponent">
       <HomeBackground />
       <SearchAndInfo />
+      <FeaturesDisplay />
+      <AskedQuestion />
+      <FooterComponent />
     </Container>
   );
 }

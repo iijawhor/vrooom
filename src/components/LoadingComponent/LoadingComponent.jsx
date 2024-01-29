61c1417be4813b294af4b568b257972be52d3fae
iijawhor <jawhorali1@gmail.com>
2024-01-29 23:04:08 +0530

    built this loading component to display lazy loading state

---
+++ src/components/LoadingComponent/LoadingComponent.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function LoadingComponent() {
+  return (
+    <div>
+      <p
+        style={{
+          color: "green",
+          fontSize: "30px",
+          height: "300px",
+          width: "100%"
+        }}
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+        consequuntur dolores, pariatur, impedit quibusdam nam error quaerat
+        earum facere beatae tempore dolor placeat quas accusantium voluptates
+        sit eum et exercitationem, rem consequatur! Natus tempore vel amet totam
+        dolorem rerum perferendis accusantium, molestias necessitatibus at
+        asperiores? Autem earum saepe est doloribus?
+      </p>
+    </div>
+  );
+}
+
+export default LoadingComponent;
